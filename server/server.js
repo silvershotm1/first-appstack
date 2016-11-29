e8e3eb865f06fce7aa618ac1cbabc1c67ca1de6a
Michael Bishop <polishthebishop@gmail.com>
2016-11-28 17:59:32 -0800

    Added URL and Router Section to the request

--- server/server.js
+++ server/server.js
@@ -41,4 +41,18 @@
 // START FUNCTION FOR HTTP MODULE
 
 var http = require('http');
+var url = require('url');
 
+function start() {
+  function onRequest(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    console.log('Request For ' + pathname + 'Received');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World');
+    res.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started');
+}
+exports.start = start;
