0210b811155ee6b7b9defbe592bbbe8600c0216b
Michael Bishop <polishthebishop@gmail.com>
2016-11-28 17:46:11 -0800

    linked server.js inside index.js

--- server/server.js
+++ server/server.js
@@ -42,15 +42,3 @@
 
 var http = require('http');
 
-function start() {
-  function onRequest(req, res) {
-    console.log('Request Received');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
-    res.end();
-  }
-
-  http.createServer(onRequest).listen(8888);
-  console.log('Server has started');
-}
-exports.start = start;
