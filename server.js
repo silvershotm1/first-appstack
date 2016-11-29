7f0d8dcdd95d2dfa0d16bb92c3b3f605cbe6a9f7
Michael Bishop <polishthebishop@gmail.com>
2016-11-28 17:26:09 -0800

    Event-Driven Async Callback

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+// INITIAL SETUP
+
+// var http = require('http');
+//
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain'});
+//   res.write('Hello World');
+//   res.end();
+// }).listen(8888);
+
+// REFACTOR WITH ASYNC
+
 var http = require('http');
 
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
+function onRequest(req, res){
+  console.log('Request Received');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World');
   res.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('The Server has Started');
